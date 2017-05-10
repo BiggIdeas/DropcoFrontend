9a3808b252af06e635fd9b65b2abd2ae07c51a3b
Stephen Rosso <stephenprosso@gmail.com>
2017-05-10 16:54:30 -0700

    last push of day

--- app/section/section.grid.controller.js
+++ app/section/section.grid.controller.js
@@ -5,7 +5,7 @@
         .module('app.core')
         .controller('SectionController', SectionController);
 
-    SectionController.$inject =  ['sectionsFactory'];
+    SectionController.$inject = ['sectionsFactory'];
 
     /* @ngInject */
     function SectionController(sectionsFactory) {
@@ -14,6 +14,10 @@
         activate();
 
         function activate() {
+            getSections();
+        }
+
+        function getSections() {
             sectionsFactory
                 .getAll()
                 .then(function(sections) {
@@ -23,6 +27,45 @@
                 })
                 .catch(function(error) {
                     console.log(error);
+                });
+        }
+        vm.searchSection = function searchSection() {
+
+            console.log("hi");
+        }
+        vm.addNewSection = function addNewSection() {
+
+            vm.addingNewSection = true;
+        }
+        vm.saveNewSection = function saveNewSection() {
+            sectionsFactory
+                .create(vm.newSection)
+                .then(function(newSection) {
+                    getProducts();
+                    vm.addingNewSection = false;
+                    clearControls();
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+        vm.cancelSaveNewSection = function cancelSaveNewSection() {
+            vm.addingNewSection = false;
+            clearControls();
+        }
+        function clearControls(){
+            vm.newSection.sectionName = '';
+            vm.newSection.departmentName = '';
+
+        }
+        function remove(product) {
+            sectionsFactory
+                .remove(section.id)
+                .then(function() {
+                    vm.sections.splice(vm.sections.indexOf(section), 1);
+                })
+                .catch(function(error) {
+                    console.error(error);
                 });
         }
     }
